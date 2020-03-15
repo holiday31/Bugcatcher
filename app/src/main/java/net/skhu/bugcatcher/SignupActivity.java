80727b3eebd7f99bbace64ba78b5feca80514b8d
jimin <holiday31@naver.com>
2020-03-15 16:00:00 +0900

    add signup

--- app/src/main/java/net/skhu/bugcatcher/SignupActivity.java
+++ app/src/main/java/net/skhu/bugcatcher/SignupActivity.java
@@ -2,12 +2,12 @@ package net.skhu.bugcatcher;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -15,9 +15,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
@@ -26,7 +33,9 @@ public class SignupActivity extends AppCompatActivity {
     private static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{4,16}$");
 
     // 파이어베이스 인증 객체 생성
-    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference ref = firebaseDatabase.getReference();
 
     private EditText inputEmail;
     private EditText inputPw;
@@ -46,14 +55,11 @@ public class SignupActivity extends AppCompatActivity {
     private boolean phoneCheck=false;
 
     String errorMessage = "";
-    DatabaseReference userInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
-        // 파이어베이스 인증 객체 선언
-        firebaseAuth = FirebaseAuth.getInstance();
 
         inputEmail = findViewById(R.id.inputEmail);
         inputPw = findViewById(R.id.inputPw);
@@ -62,9 +68,89 @@ public class SignupActivity extends AppCompatActivity {
         inputName = findViewById(R.id.inputName);
         radioGroup1= findViewById(R.id.radioGroup1);
 
-        //userInfo = FirebaseDatabase.getInstance().getReference("userInfo");
 
-        //userInfo.addValueEventListener(listener1);
+
+        Button emailCheckbtn=findViewById(R.id.emailCheck);
+        emailCheckbtn.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ref.child("users").addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                boolean hasEmail=false;
+                                email = inputEmail.getText().toString();
+                                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                        Log.d("snapshot:",snapshot.child("email").getValue(String.class));
+
+                                        if(snapshot.child("email").getValue(String.class).equals(email))
+                                        {
+                                            hasEmail= true;
+                                            break;
+                                        }
+                                    }
+                                if(hasEmail)
+                                    Toast.makeText(SignupActivity.this, R.string.id_check_fail, Toast.LENGTH_SHORT).show();
+                                else{
+                                    idCheck=true;
+                                    Toast.makeText(SignupActivity.this, R.string.id_check_success, Toast.LENGTH_SHORT).show();
+                                    inputEmail.setClickable(false);
+                                    inputEmail.setFocusable(false);
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+
+
+                    }
+                }) ;
+        Button phoneCheckbtn=findViewById(R.id.phoneCheck);
+        phoneCheckbtn.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ref.child("users").addValueEventListener(new ValueEventListener() {
+                            //전화번호 중복확인 완료 후 전화번호 바꿀 시 중복확인 통과 오류 고치기
+                            //새로 창을 띄워서 그안에서 아이디 중복확인을 해야할 것 같다.
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                boolean hasPhone=false;
+                                phone = inputPhone.getText().toString();
+                                Iterator<DataSnapshot> child = dataSnapshot.getChildren().iterator();
+                                while(child.hasNext())
+                                {
+                                    if(child.next().getKey().equals(phone))
+                                    {
+                                        hasPhone= true;
+                                        break;
+                                    }
+                                }
+                                if(hasPhone)
+                                    Toast.makeText(SignupActivity.this, R.string.phone_check_fail, Toast.LENGTH_SHORT).show();
+                                else{
+                                    phoneCheck=true;
+                                    Toast.makeText(SignupActivity.this, R.string.phone_check_success, Toast.LENGTH_SHORT).show();
+                                    inputPhone.setClickable(false);
+                                    inputPhone.setFocusable(false);
+                                }
+
+                            }
+
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }) ;
+
     }
 
 
@@ -84,10 +170,10 @@ public class SignupActivity extends AppCompatActivity {
 
         if(isValidInfo()) {
             createUser(email, password);
-            //writeNewUser(email,name,phone,sex);
-            //Intent signUpIntent=new Intent(this, MainActivity.class);
-            //startActivity(signUpIntent);
-
+            writeNewUser(email,name,phone,sex);
+            //Toast.makeText(SignupActivity.this, "사용자 등록 성공", Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(this, MainActivity.class);
+            startActivity(intent);
         }
         else{
             Toast.makeText(SignupActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
@@ -96,73 +182,80 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void setIdCheck(View view) {
-        this.idCheck=true;
-        Toast.makeText(SignupActivity.this, R.string.id_check_success, Toast.LENGTH_SHORT).show();
+
+
     }
 
     public void setPhoneCheck(View view) {
-        this.phoneCheck=true;
-        Toast.makeText(SignupActivity.this, R.string.phone_check_success, Toast.LENGTH_SHORT).show();
-    }
 
-    // 정보 유효성 검사
-    private boolean isValidInfo() {
-
-         if(name.isEmpty()){
-            errorMessage="이름을 입력하세요.";
-        }
-        // 이메일 공백, 이메일 형식 불일치
-        else if ((email.isEmpty())||(!Patterns.EMAIL_ADDRESS.matcher(email).matches())){
-            errorMessage="이메일을 올바르게 입력하세요.";
-        }
-        else if(password.isEmpty()||(!PASSWORD_PATTERN.matcher(password).matches())){
-            errorMessage="패스워드를 올바르게 입력하세요.";
-        }
-        else if(pwcheck.isEmpty()||password.equals(passwordCheck)){
-            errorMessage="패스워드가 일치하지 않습니다.";
-        }
-        else if(phone.isEmpty()||(!Patterns.PHONE.matcher(phone).matches())){
-             errorMessage="휴대폰번호를 올바르게 입력하세요.";
-         }
-         else if(sex.isEmpty()){
-             errorMessage="성별을 선택하세요.";
-         }
-         else if(!idCheck){
-             errorMessage="아이디 중복확인을 하세요";
-         }
-         else if(!phoneCheck){
-             errorMessage="휴대폰 번호 중복확인을 하세요";
-         }
-        else{
-            return true;
-        }
-        return false;
     }
 
 
 
-    // 회원가입
-    private void createUser(String email, String password) {
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // 회원가입 성공
+      // 정보 유효성 검사
+      private boolean isValidInfo() {
 
-                            Toast.makeText(SignupActivity.this, R.string.success_signup, Toast.LENGTH_SHORT).show();
-                        } else {
-                            // 회원가입 실패
-                            Toast.makeText(SignupActivity.this, R.string.failed_signup, Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
+           if(name.isEmpty()){
+              errorMessage="이름을 입력하세요.";
+          }
+          // 이메일 공백, 이메일 형식 불일치
+          else if ((email.isEmpty())||(!Patterns.EMAIL_ADDRESS.matcher(email).matches())){
+              errorMessage="이메일을 올바르게 입력하세요.";
+          }
+          else if(password.isEmpty()||(!PASSWORD_PATTERN.matcher(password).matches())||(password.length()<6)){
+              errorMessage="패스워드를 올바르게 입력하세요.";
+          }
+          else if(pwcheck.isEmpty()||password.equals(passwordCheck)){
+              errorMessage="패스워드가 일치하지 않습니다.";
+          }
+          else if(phone.isEmpty()||(!Patterns.PHONE.matcher(phone).matches())){
+               errorMessage="휴대폰번호를 올바르게 입력하세요.";
+           }
+           else if(sex.isEmpty()){
+               errorMessage="성별을 선택하세요.";
+           }
+           else if(!idCheck){
+               errorMessage="아이디 중복확인을 하세요";
+           }
+           else if(!phoneCheck){
+               errorMessage="휴대폰 번호 중복확인을 하세요";
+           }
+          else{
+              return true;
+          }
+          return false;
+      }
 
-    //개인정보저장
-    private void writeNewUser(String email,String name, String phone, String sex) {
-        UserInfo user = new UserInfo(email,name,phone,sex);
-        userInfo.child("users").child(email).setValue(user);
-    }
+
+
+
+      // 회원가입
+      private void createUser(String email, String password) {
+          firebaseAuth.createUserWithEmailAndPassword(email, password)
+                  .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                      @Override
+                      public void onComplete(@NonNull Task<AuthResult> task) {
+                          if (task.isSuccessful()) {
+                              // 회원가입 성공
+                              Toast.makeText(SignupActivity.this, R.string.success_signup, Toast.LENGTH_SHORT).show();
+
+                          } else {
+                              // 회원가입 실패
+                              Toast.makeText(SignupActivity.this, R.string.failed_signup, Toast.LENGTH_SHORT).show();
+                          }
+                      }
+                  });
+      }
+
+
+      //개인정보저장
+      private void writeNewUser(String email,String name, String phone, String sex) {
+          UserInfo user = new UserInfo(email,name,phone,sex);
+          ref.child("users").child(phone).setValue(user);
+      }
+
+
+
+
 
 }
