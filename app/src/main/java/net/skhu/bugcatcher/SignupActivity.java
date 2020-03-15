8aaf551e9c954147affc77107c6893a436f7dfe2
jimin <holiday31@naver.com>
2020-03-15 15:57:01 +0900

    add signup and login

--- app/src/main/java/net/skhu/bugcatcher/SignupActivity.java
+++ app/src/main/java/net/skhu/bugcatcher/SignupActivity.java
@@ -3,8 +3,7 @@ package net.skhu.bugcatcher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
@@ -12,17 +11,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
-
-import com.firebase.ui.auth.data.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 import java.util.regex.Pattern;
 
